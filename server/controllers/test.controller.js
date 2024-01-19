cc27d0c6743107b2cf6d50d54b343ce98ef45aa4
Sandu70one <sanduaruna70one@gmail.com>
2024-01-19 10:06:32 +0530

    Create test api route

---
+++ server/controllers/test.controller.js
@@ -0,0 +1,6 @@
+export const test = (req, res) => {
+  res.json({
+    message: "test api route is working properly...",
+  });
+};
+export default test;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { error } from "console";
 
+//Routes
+import testRouter from "./routes/test.route.js";
+
+// import userRoutes from "./routes/user.routes.js";
+// import postRoutes from "./routes/postRoutes.js";
+// import commentRoutes from "./routes/commentRoutes.js";
+// import likeRoutes from "./routes/likeRoutes.js";
+// import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.mongoURL)
@@ -18,3 +27,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is up on port 3000");
 });
+
+app.use("/api/path", testRouter);
